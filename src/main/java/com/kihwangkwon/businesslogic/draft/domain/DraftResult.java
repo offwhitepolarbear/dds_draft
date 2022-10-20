932c33ab90d3236b27cc0c597e4947af76cf0ae5
OffwhitePolarBear <kgh0618@hanmail.net>
2022-10-20 17:54:51 +0900

    비딩 작업중

--- src/main/java/com/kihwangkwon/businesslogic/draft/domain/DraftResult.java
+++ src/main/java/com/kihwangkwon/businesslogic/draft/domain/DraftResult.java
@@ -5,12 +5,14 @@ import com.kihwangkwon.jpaaudit.BaseTimeEntity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
 @Setter
+@Entity
 public class DraftResult extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
